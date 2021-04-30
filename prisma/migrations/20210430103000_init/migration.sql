4a7b004f97f2edcbc173618ec3e573a0482466bc
Cali Castle <cali@calicastle.com>
2021-04-30 18:32:46 +0800

    Add prisma + init migration

---
+++ prisma/migrations/20210430103000_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `firstname` VARCHAR(191),
+    `lastname` VARCHAR(191),
+    `role` ENUM('ADMIN', 'USER') NOT NULL,
+UNIQUE INDEX `User.email_unique`(`email`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `published` BOOLEAN NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191),
+    `authorId` VARCHAR(191),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
